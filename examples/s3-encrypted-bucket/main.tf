dc5680fe1d69a6fe141e52a859ca8031359fe07d
Rafał Masiarek <rafal@masiarek.pl>
2022-07-10 14:25:59 +0200

    use in encryption example own module [Infrastrukturait/kms-key/aws] in `v0.1.0` version

--- examples/s3-encrypted-bucket/main.tf
+++ examples/s3-encrypted-bucket/main.tf
@@ -13,9 +13,14 @@ module "app_prod_bastion_label" {
   }
 }
 
-resource "aws_kms_key" "app_prod_bastion_bucket_key" {
+module "app_prod_bastion_bucket_key" {
+  source                  = "Infrastrukturait/kms-key/aws"
+  version                 = "0.1.0"
   description             = "This key is used to encrypt bucket objects"
   deletion_window_in_days = 10
+  enable_key_rotation     = false
+
+  tags                    = module.app_prod_kms_label.tags  
 }
 
 module "app_prod_bucket" {
@@ -23,6 +28,6 @@ module "app_prod_bucket" {
     bucket_name                 = join(module.app_prod_bastion_label.delimiter, [module.app_prod_bastion_label.stage, module.app_prod_bastion_label.name, var.name])
     bucket_acl                  = var.bucket_acl
     encryption_enabled          = true
-    encryption_master_kms_key   = aws_kms_key.app_prod_bastion_bucket_key.arn
+    encryption_master_kms_key   = module.app_prod_bastion_bucket_key.key_arn
     tags                        = module.app_prod_bastion_label.tags
 }
